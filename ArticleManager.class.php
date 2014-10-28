3a14ed7f8545d839feea2e1fe5b7cc383a328ca5
Mika <webdeveloper@diw.fr>
2014-10-28 11:24:37 +0100

    modifies functions of article manager

--- ArticleManager.class.php
+++ ArticleManager.class.php
@@ -35,40 +35,58 @@ class ArticleManager{
         return new Article($result);
     }
     
+    public function getAllArticles(){
+        $LIST = $this->_db->query('SELECT *, '
+                . 'DATE_FORMAT(date, \'%d/%m/%y\') AS date '
+                . 'FROM article '
+                . 'ORDER BY date '
+                . 'DESC');
+        /* A chaque fois que la condition est vrai, la boucle while ajoute un item
+         * dans le tableau Articles */
+        while($list_fetch = $LIST->fetch(PDO::FETCH_ASSOC)){
+            $articles[] = new Article($list_fetch);
+        }
+        return $articles;
+        $LIST->closeCursor();
+    }
+    
     public function addArticle(Article $article) {
         $ADD_ARTICLE = $this->_db->prepare('INSERT INTO article '
                 . 'SET title=:title, id_user=:id_user, date=NOW(), image=:image, '
-                . 'message=:message');
-	$ADD_ARTICLE->execute(array(
-            ':title' => htmlspecialchars($article->getTitle()),
-            ':message' => htmlspecialchars($article->getMessage()),
-	));
-        $ADD_ARTICLE->bindParam(':title', $article->getTitle());
+                . 'message=:message, published=:published');
+        $ADD_ARTICLE->bindValue(':title', $article->getTitle());
+	$ADD_ARTICLE->bindValue(':id_user', $article->getId_user());
+	$ADD_ARTICLE->bindValue(':image', $article->getImage());
+	$ADD_ARTICLE->bindValue(':message', $article->getMessage());
+	$ADD_ARTICLE->bindValue(':message', $article->getMessage());
+        $ADD_ARTICLE->bindValue(':published', (int) $article->getPublished() );
+	$ADD_ARTICLE->execute();
 	$ADD_ARTICLE->closeCursor();
 	}
     
     public function updateArticle(Article $article) {
         $UP_ARTICLE = $this->_db->prepare('UPDATE article '
-                . 'SET title=:title, id_user=:id_user, image=:image, message=:message '
-                . 'WHERE id=:id');
-	$UP_ARTICLE->bindValue(':id', $article->getId());
-	$UP_ARTICLE->bindValue(':title', $article->getTitle());
+                . 'SET title=22, id_user=:id_user, image=:image, message=:message, '
+                . 'published=:published '
+                . 'WHERE id_article=:id_article');
+	$UP_ARTICLE->bindValue(':id_article', $article->getId_article());
+	// $UP_ARTICLE->bindValue(':title', $article->getTitle());
 	$UP_ARTICLE->bindValue(':id_user', $article->getId_user());
 	$UP_ARTICLE->bindValue(':image', $article->getImage());
 	$UP_ARTICLE->bindValue(':message', $article->getMessage());
-	
+	$UP_ARTICLE->bindValue(':published', (int) $article->getPublished());
 	$UP_ARTICLE->execute();
 	$UP_ARTICLE->closeCursor();
 	}
     
     public function deleteArticle($id){
         $id = (int) $id;
-        $ARTICLE = $this->_db->prepare('DELETE * '
+        $DEL_ARTICLE = $this->_db->prepare('DELETE '
                 . 'FROM article '
-                . 'WHERE id=:id');
-        $ARTICLE->bindValue(':id', $id);
-        $ARTICLE->execute();
-	$UP_ARTICLE->closeCursor();
+                . 'WHERE id_article=:id_article');
+        $DEL_ARTICLE->bindValue(':id_article', $id);
+        $DEL_ARTICLE->execute();
+	$DEL_ARTICLE->closeCursor();
         print "Article supprimé";
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,9 @@ require_once 'Article.class.php';
 require_once 'ArticleManager.class.php';
 
 $manager = new ArticleManager($DB);
-$article = $manager-> getArticle(2);
-
+$article = $manager-> getArticle(1);
+// $manager-> addArticle($article);
+$manager-> updateArticle($article);
+ 
+// $article = $manager-> getAllArticles();
 debug($article);
